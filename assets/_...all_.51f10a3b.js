58008fc116a40f5123e94c28b3ac614ef121dcd2
SoonIter <SoonIter@users.noreply.github.com>
2022-10-05 15:23:32 +0000

    deploy: 96df2f6f71cc335b32f709dba65fa749a90b3173

---
+++ assets/_...all_.51f10a3b.js
@@ -0,0 +1,1 @@
+import{d as e,a as o,o as a,c as s,t as c,f as r,G as n}from"./app.f67703f3.js";const f=e({__name:"[...all]",setup(l){const{t}=o();return(p,u)=>(a(),s("div",null,c(r(t)("not-found")),1))}});typeof n=="function"&&n(f);export{f as default};
