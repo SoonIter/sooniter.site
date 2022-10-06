2350e22e477e25484761a1113881992b548cc885
YeanSe <1730214056@qq.com>
2022-10-07 02:14:23 +0800

    chore:add CNAME file.
    To protect from the custom domain clean.

--- src/components.d.ts
+++ src/components.d.ts
@@ -8,7 +8,6 @@ export {}
 declare module '@vue/runtime-core' {
   export interface GlobalComponents {
     About: typeof import('./components/Icons/About.vue')['default']
-    AboutMe: typeof import('./components/AboutMe.md')['default']
     AriticleList: typeof import('./components/AriticleList.vue')['default']
     ArticleCard: typeof import('./components/ArticleCard.vue')['default']
     Avatar: typeof import('./components/Avatar.vue')['default']
@@ -27,8 +26,6 @@ declare module '@vue/runtime-core' {
     Post: typeof import('./components/Post.vue')['default']
     ProfileCard: typeof import('./components/ProfileCard.vue')['default']
     Project: typeof import('./components/Project.vue')['default']
-    README: typeof import('./components/README.md')['default']
-    'README.zh_CN': typeof import('./components/README.zh_CN.md')['default']
     RouterLink: typeof import('vue-router')['RouterLink']
     RouterView: typeof import('vue-router')['RouterView']
     ScrollToTop: typeof import('./components/ScrollToTop.vue')['default']
