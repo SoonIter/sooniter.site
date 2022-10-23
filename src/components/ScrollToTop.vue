b1f4467f8b6ffd187f6bca1842d8fdd17bf78a35
YeanSe <1730214056@qq.com>
2022-10-23 17:30:20 +0800

    feat:scrollToTop上下出现

--- src/components/ScrollToTop.vue
+++ src/components/ScrollToTop.vue
@@ -6,11 +6,22 @@ const scrollToTop = () => {
 </script>
 
 <template>
-  <button
-    v-show="y > 200" i-carbon-mac-shift text-gray hover:text-dark hover:dark-text-white hover:shadow-xl w-10
-    h-10 transition-all text-sm rd-full fixed bottom-8 right-8
-    select-none
-    @click="scrollToTop()"
-  />
+  <Transition>
+    <button
+      v-show="y > 200" i-carbon-mac-shift text-gray hover:text-dark hover:dark-text-white hover:shadow-xl w-10
+      h-10 text-sm rd-full fixed bottom-8 right-8 select-none @click="scrollToTop()"
+    />
+  </Transition>
 </template>
 
+<style scoped>
+.v-enter-active,
+.v-leave-active {
+  transition: all 0.2s ease;
+}
+
+.v-enter-from,
+.v-leave-to {
+  transform: translateY(100px);
+}
+</style>
